5aa3070084d4c076d259c84cc1990d7083bffd67
Juan G Lara Gómez <piedradesales@gmail.com>
2023-06-19 20:20:44 +0200

    Java y Phyton

---
+++ Práctica finalizada GUI 22_23/.metadata/python/Hello.py
@@ -0,0 +1,31 @@
+# Esto es un comentario
+# Nuestro hola mundo en Python
+
+
+#  Cadena de texto  con comulla simple o doble (es lo mismo)
+print("Hola Python")
+print('Hola Python')
+
+# Consultar el tipo de dato 
+print(type(12)) #Tipo 'int'
+print(type(12.5)) #Tipo 'float'
+print(type(True)) #Tipo 'boolean'
+print(type(1 + 3j)) # Tipo'dato complejo'
+
+
+"""
+Este es un 
+comentario en varias 
+líneas
+
+"""
+
+'''
+Este es un 
+comentario en varias 
+líneas
+
+'''
+
+
+
