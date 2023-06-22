7a47566a409c448def47a8b26adebe3ca73b8705
JG <piedradesales@gmail.com>
2023-06-22 22:03:02 +0200

    22/06_22:02

---
+++ python/02_operators.py
@@ -0,0 +1,48 @@
+### operadores  aritméticos ###
+### Python es un lenguaje que se usa mucho para calculos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(5 % 4)
+print(10 // 4) # Floor división ( da número entero )
+print(2 ** 3) # Calcula exponente ( potencias elevado a ... )
+print(2 ** + 3 + 3 - 7  / 1 // 4 )
+
+# Concatenación
+print("Hola" + " Python "+" ¿ qué tal ? ")
+# Concatenar números con str
+print("Hola", str(5))
+# ERROR print("Hola" + 5)
+
+print("Hola " * 5) # el operador * mutiplica el contenido del print por 'n' veces
+print("Hola " * (2 ** 3)) # lo multiplica siempre que sea entero el resultado
+
+my_int  = 2.5  * 2
+print("Hola " *int(my_int)) # casteo de un número decimal a entero con int()
+
+### operadores comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 < 4 > 2)
+
+# comparación de cadenas ( compara la ordenación alfabética por ASCII )
+
+print(" Hola " > " Python")
+print(" Hola " < " Python ")
+print(" Hola " >= " Python ")
+print(" Hola " <= " Python ")
+print(" Hola " == " Python ")
+print(" Hola " != " Python ")
+
+print(len("Hola") < len("Adiós"))
+
+
+### operadores lógicos ###
+
