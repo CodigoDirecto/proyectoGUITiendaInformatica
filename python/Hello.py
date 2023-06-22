eeb73676a973c0d9a11d1f9993366d04398b106e
JG <piedradesales@gmail.com>
2023-06-22 20:29:52 +0200

    22/06 20:29

--- python/Hello.py
+++ python/Hello.py
@@ -12,7 +12,7 @@ print(type(12.5)) #Tipo 'float'
 print(type(True)) #Tipo 'boolean'
 print(type(1 + 3j)) # Tipo'dato complejo'
 
-
+print(type(print("Mi cadena de texto"))) # Tipo "None type"
 """
 Este es un 
 comentario en varias 
