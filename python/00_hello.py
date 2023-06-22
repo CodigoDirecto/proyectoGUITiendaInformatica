8e803f3aaebea40e35d0a9e1e6de2f59070e6374
JG <piedradesales@gmail.com>
2023-06-22 19:09:42 +0200

    día 22

---
+++ python/00_hello.py
@@ -0,0 +1,13 @@
+# Variables
+# Nomenclatura "Snake Case" y no "notacion camello" 
+my_variable = "My String variable"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# argumentos separados por comas ( crea una cadena )
+print(my_variable,my_int_variable,my_bool_variable)
