901eb6c33f108fb102a09c8c802b9ecaf065b558
JG <piedradesales@gmail.com>
2023-06-22 19:45:05 +0200

    22/06

--- python/00_hello.py
+++ python/00_hello.py
@@ -6,8 +6,15 @@ print(my_variable)
 my_int_variable = 5
 print(my_int_variable)
 
+# transformar en una cadena de texto con str()
+my_int_to_str_variable = str(my_int_variable)
+
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
 my_bool_variable = False
 print(my_bool_variable)
 
 # argumentos separados por comas ( crea una cadena )
-print(my_variable,my_int_variable,my_bool_variable)
+print(my_variable,str(my_int_variable),my_bool_variable)
+
