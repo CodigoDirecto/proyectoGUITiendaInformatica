fbc6133ec882e215a050856a6105c3a953c7734f
JG <piedradesales@gmail.com>
2023-06-28 22:46:51 +0200

    28/06/23_08:54

--- python/11_clases.py
+++ python/11_clases.py
@@ -16,20 +16,24 @@ class Person:
     # self es el propio objeto ( se puede crear el objeto con valores literarles y parámetros vacíos )
     
     def __init__ (self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.full_name = f"{self.name} {self.surname}"
+        self.full_name = f"{name} {surname}"
+        # se hacen privados los atributos con __
+        self.__name = name
+        self.__surname = surname
     # Se pueden definir métodos|funciones ( acciones que puede hacer )
     def walk(self):
         print(f"{self.full_name} está caminando")
 
 
 my_person = Person("Colibrí" ,"Desarrollador")
-print(f"{my_person.name} {my_person.surname}")
+# Una vez hechos privados los atributos ya no podemos accedes de otra forma que con getters y setters
+# print(f"{my_person.name} {my_person.surname}")
 
 # Llamada a la función del objeto ( nombre del objeto.nombre de la función)
 my_person.walk()
 
 # Podemos asignar valores a los atributos del objeto con el operador = y el operador . ( nombreObjeto.nombreAtributo = "nuevo valor" )
 my_person.full_name = "Hétor de León ( el loco de los perros )"
+# Este atributo al no ser privado se puede acceder sin get ni set
 print(f"{my_person.full_name}")
+
